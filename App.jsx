c1014c2a83344edc0086c48b521388ac2a180e03
Haroldminv <haroldmachadoinv@gmail.com>
2025-04-22 12:19:48 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Bienvenido a Tpips</h1>
+      <p>Tu fuente de indicadores económicos en tiempo real.</p>
+    </div>
+  )
+}
+
+export default App
